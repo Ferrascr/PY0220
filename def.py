ecc555f9ccbcc1386f6ec56623f296e4b2c35d70
Esteban Chaves Montes <echavesm@mail.sitiox.com>
2020-04-22 19:27:28 -0600

    Correccion

--- def.py
+++ def.py
@@ -1,6 +1,6 @@
 from calc import *
 
-def ec(o) :
+def ecm(o) :
 	if o != "+" and o != "-" and o != "*" and o != "/" :
 		valido = False
 	else:
@@ -10,24 +10,25 @@ def ec(o) :
 print("Bienvenido a la calculadora")
 nombre = input("Escriba su usuario ")
 operacion = input("Indique la operacion (+, -, *, /): ")
-while ec(operacion) == False:
+while ecm(operacion) == False:
 	print("No es una operacion valida")
 	operacion = input("Indique la operacion (+, -, *, /): ")
 else:
 	oper1 = input("Indique el primer numero:")
 	oper2 = input("Indique el segundo numero:")
-if ec(oper1) and ec(oper2) :
-    oper1 = int(oper1)
-    oper2 = int(oper2)
-    if operacion == "+" :
-        print(suma(oper1,oper2))
-    elif operacion == "-" :
-        print(resta(oper1, oper2))
-    elif operacion == "*" :
-        print(multiplicacion(oper1, oper2))
-    elif operacion == "/" :
-        print(division(oper1, oper2))
-    else :
-        print("Error! Eso no es una operación válida!")
-else :
-        print("Error! Ambos números deben tener formato numérico.")
+ 
+	if oper1.isnumeric() == True and oper2.isnumeric() == True:
+		oper1 = int(oper1)
+		oper2 = int(oper2)
+		if operacion == "+" :
+			print(suma(oper1, oper2))
+		elif operacion == "-" :
+			print(resta(oper1, oper2))
+		elif operacion == "*" :
+			print(multiplicacion(oper1, oper2))
+		elif operacion == "/" :
+			print(division(oper1, oper2))
+		else :
+			print("No es una operacion valida")
+	else :
+		print("Error! Ambos digitos deben tener formato numerico")
